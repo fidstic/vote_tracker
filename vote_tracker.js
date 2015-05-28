0760dabee9c0fd1d4f8ae469cd39515666d724d8
Matt Herron <Mdherron@gmail.com>
2015-05-28 15:56:48 -0700

    able to get ajax and JSON to produce random image from my imgur account

--- vote_tracker.js
+++ vote_tracker.js
@@ -1,56 +1,99 @@
-$(function (){
 
-  // var photos = [
-  // '01.jpg',
-  // '02.jpg',
-  // '03.jpg',
-  // '04.jpg',
-  // '05.jpg',
-  // '06.jpg',
-  // '07.jpg',
 
-  // ];
+var Cat = function(url) {
+  this.votes = 0;
+  this.picUrl = url;
+};
 
-  // var photosX = [
-  // '08.jpg',
-  // '09.jpg',
-  // '10.jpg',
-  // '11.jpg',
-  // '12.jpg',
-  // '13.jpg',
-  // '14.jpg',
+var Game = function() {
+  this.rounds = 0;
+}
 
-  // ];
+var picsArray = [];
 
-//de duplicate
+var myGame = new Game();
 
-  var Cat = function (photo, vote) {
-    this.photo = photo
-    this.vote = vote
+$.ajax({
+  url: "https://api.imgur.com/3/album/LGiPx",
+  headers:{
+    "Authorization": "Client-ID 10328f45ce47c77"
     }
-
-  var catArray = [];
-
-  for (var i = 0; i <14; i++){
-    catArray.push(new Cat(catArray[i], 0))
-  };
-
-  var
-
-
-  $('<img src="images/' + [Math.floor(Math.random() * catArray.length) +1] + '.jpg" ' + 'height="300"' + '>').appendTo('#photo1');
-  $('<img src="images/' + [Math.floor(Math.random() * catArray.length) +1] + '.jpg" ' + 'height="300"' + '>').appendTo('#photo2');
-
-  var Tracker = function()
-
-
-
-
-  $('td.total1').text(Math.floor(Math.random() * (5 - 1) +1 )).hide();
-  $('td.total2').text(Math.floor(Math.random() * (5 - 1) +1 )).hide();
-
-
+  })
+  .done(function(res) {
+    var images = res.data.images;
+    for (i = 0; i < images.length; i++){
+      picsArray.push(new Cat(images[i].link));
+    };
+    myGame.insertCatPic();
   });
+
+Game.prototype.insertCatPic = function () {
+  var randIndex1 = Math.floor(Math.random() * picsArray.length);
+  var randIndex2 = Math.floor(Math.random() * picsArray.length);
+  console.log(randIndex1 + "  " + randIndex2);
+  while (randIndex1 === randIndex2) {
+    randIndex2 = Math.floor(Math.random() * picsArray.length - 1);
+  }
+
+  $('#kit1').attr('src', picsArray[randIndex1].picUrl);
+  $('#kit2').attr('src', picsArray[randIndex2].picUrl);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Make object constructor for images Cat
+//then make an anonymous function that creates cat images from
+
+
+
+//when you call an anonymous function inside jquery and waits until everything else is ready to run
+
+
+   //$('<img src="images/' + [Math.floor(Math.random() * catArray.length) +1] + '.jpg" ' + 'height="300"' + '>').appendTo('#photo1');
+   //$('<img src="images/' + [Math.floor(Math.random() * catArray.length) +1] + '.jpg" ' + 'height="300"' + '>').appendTo('#photo2');
+
+   //var Tracker = function()
+
+  //$('td.total1').text(Math.floor(Math.random() * (5 - 1) +1 )).hide();
+  //$('td.total2').text(Math.floor(Math.random() * (5 - 1) +1 )).hide();
+
+
+   //  function imgNum(number1, number2, max){
+   //   var newNumber;
+   //   do{
+   //     newNumber = Math.floor(Math.random() * max);
+   //   }
+   //   while (newNumber === number1 || newNumber === number2);
+   //   return newNumber;
+   // };
+
+
+
 //tracker function. You have to run a comparison statement between the image and array number then add vote to this.vote
 
 //tertiary operator this.id == button1 ? button2 : button1 if this do this if else do this
